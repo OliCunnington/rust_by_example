1c16b84de8f6d22463a14994b0cc704f1723840f
Oliver Cunnington <ofcunnington@hotmail.com>
2026-08-23 13:24:53 +0200

    display

--- display_impl.rs
+++ display_impl.rs
@@ -46,6 +46,18 @@ impl fmt::Display for Point2D {
     }
 }
 
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {:+}i", self.real, self.imag)
+    }
+}
+
 fn main() {
     let minmax = MinMax(0, 14);
 
@@ -70,4 +82,10 @@ fn main() {
     // were implemented, but `{:b}` requires `fmt::Binary` to be
     // implemented, which it hasn't been for `Point2D`.
     // println!("What does Point2D look like in binary: {:b}?", point);
+
+    let complex = Complex { real: 3.3, imag: -7.2 };
+
+    println!("Compare complex:");
+    println!("Display: {}", complex);
+    println!("Debug: {:?}", complex);
 }
